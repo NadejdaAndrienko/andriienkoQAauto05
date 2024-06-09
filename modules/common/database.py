40e8afce2ab67c90e44cdece3625d8cb32d7dfb6
Nadii Andriienko <nadejda.andrienko.86@gmail.com>
2024-06-09 19:06:59 +0000

    Added updates to database tests

--- modules/common/database.py
+++ modules/common/database.py
@@ -3,46 +3,69 @@ import sqlite3
 
 class Database():
     def __init__(self):
-        self.connection = sqlite3.connect(r'C:\\Users\\Administrator\\andriienkoQAauto05' + r'\\become_qa_auto.db')
+        self.connection = sqlite3.connect(
+            r'C:\\Users\\Administrator\\andriienkoQAauto05' +
+            r'\\become_qa_auto.db')
+
         self.cursor = self.connection.cursor()
 
     def tets_connection(self):
         sqlite_select_Query = "SELECT sqlite_version();"
+
         self.cursor.execute(sqlite_select_Query)
         record = self.cursor.fetchall()
+
         print(f"Connected successfully. SQLite Database Version is {record}")
 
     def get_all_users(self):
-        query = "SELECT name, address, city FROM customers"
+        query = "SELECT name, address, city \
+                FROM customers"
+
         self.cursor.execute(query)
         record = self.cursor.fetchall()
+
         return record
-    
+
     def get_user_address_by_name(self, name):
-        query = f"SELECT address, city, postalCode, country FROM customers WHERE name = '{name}'"
+        query = f"SELECT address, city, postalCode, country \
+            FROM customers \
+            WHERE name = '{name}'"
+
         self.cursor.execute(query)
         record = self.cursor.fetchall()
+
         return record
-    
+
     def update_product_qnt_by_id(self, product_id, qnt):
-        query = f"UPDATE products SET quantity = {qnt} WHERE id = {product_id}"
+        query = f"UPDATE products \
+                SET quantity = {qnt} \
+                WHERE id = {product_id}"
+
         self.cursor.execute(query)
         self.connection.commit()
 
     def select_product_qnt_by_id(self, product_id):
-        query = f"SELECT quantity FROM products WHERE id = {product_id}"
+        query = f"SELECT quantity \
+                FROM products \
+                WHERE id = {product_id}"
+
         self.cursor.execute(query)
         record = self.cursor.fetchall()
+
         return record
-    
+
     def insert_product(self, product_id, name, descriprion, qnt):
-        query = f"INSERT OR REPLACE INTO products(id, name, description, quantity) \
+        query = f"INSERT OR REPLACE INTO products \
+            (id, name, description, quantity) \
             VALUES({product_id}, '{name}', '{descriprion}', {qnt})"
+
         self.cursor.execute(query)
         self.connection.commit()
 
     def delete_product_by_id(self, product_id):
-        query = f"DELETE FROM products WHERE id = {product_id}"
+        query = f"DELETE FROM products \
+            WHERE id = {product_id}"
+
         self.cursor.execute(query)
         self.connection.commit()
 
@@ -52,6 +75,8 @@ class Database():
                 FROM orders \
                 JOIN customers ON orders.customer_id = customers.id \
                 JOIN products ON orders.product_id = products.id"
+
         self.cursor.execute(query)
         record = self.cursor.fetchall()
+
         return record
--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -20,7 +20,7 @@ def test_check_all_users():
 def test_check_user_sergii():
     db = Database()
     user = db.get_user_address_by_name('Sergii')
-    
+
     assert user[0][0] == 'Maydan Nezalezhnosti 1'
     assert user[0][1] == 'Kyiv'
     assert user[0][2] == '3127'
@@ -32,7 +32,7 @@ def test_product_qnt_update():
     db = Database()
     db.update_product_qnt_by_id(1, 25)
     water_qnt = db.select_product_qnt_by_id(1)
-    
+
     assert water_qnt[0][0] == 25
 
 
@@ -41,7 +41,7 @@ def test_product_insert():
     db = Database()
     db.insert_product(4, 'печиво', 'солодке', 30)
     water_qnt = db.select_product_qnt_by_id(4)
-    
+
     assert water_qnt[0][0] == 30
 
 
@@ -51,15 +51,14 @@ def test_product_delete():
     db.insert_product(99, 'тестові', 'дані', 999)
     db.delete_product_by_id(99)
     qnt = db.select_product_qnt_by_id(99)
-    
+
     assert len(qnt) == 0
 
 
-@pytest.mark.database_join
+@pytest.mark.database
 def test_detailed_orders():
     db = Database()
     orders = db.get_detailed_orders()
-    #print("Orders", orders)
 
     assert len(orders) == 1
 
@@ -67,4 +66,3 @@ def test_detailed_orders():
     assert orders[0][1] == 'Sergii'
     assert orders[0][2] == 'солодка вода'
     assert orders[0][3] == 'з цукром'
-    
